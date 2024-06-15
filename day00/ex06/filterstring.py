e23cf55741f317c3a69e9b8d5aef948e343a3f46
oumaimafisaoui <oumaimafisaoui@gmail.com>
2024-06-15 22:46:43 +0100

    ex06

--- day00/ex06/filterstring.py
+++ day00/ex06/filterstring.py
@@ -8,8 +8,8 @@ def filterString(arg, l):
 def main():
     try:
         l = len(sys.argv)
-        assert l == 3, "Wrong number of arguments"
-        assert (sys.argv[1] and sys.argv[2] and sys.argv[1] != "None" and sys.argv[2].isdigit() and sys.argv[2] != 0), "Wrong argument type"
+        assert l == 3, "the arguments are bad"
+        assert (sys.argv[1] and sys.argv[2] and sys.argv[1] != "None" and not sys.argv[1].isdigit() and sys.argv[2].isdigit() and sys.argv[2] != 0), "the arguments are bad"
         print(filterString(sys.argv[1], int(sys.argv[2])))
     except AssertionError as e:
         print(f"Assertion Error: {e}")
