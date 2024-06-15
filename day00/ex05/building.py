58745b642661e47a71fb09e759c9a462a6ea6440
oumaimafisaoui <oumaimafisaoui@gmail.com>
2024-06-15 20:06:43 +0100

    ex05

---
+++ day00/ex05/building.py
@@ -0,0 +1,42 @@
+import sys
+
+def building(arg):
+        upper = lower= digits=  punc = space = 0
+        arg = arg
+        l = len(arg)
+        for i in range(len(arg)):
+            if(arg[i] >= 'a' and arg[i] <= 'z'):
+                lower += 1
+            elif(arg[i] >= 'A' and arg[i] <= 'Z'):
+                upper +=1
+            elif(not arg[i].isalnum() and not arg[i].isspace()):
+                punc += 1
+            elif (arg[i].isspace()):
+                space += 1
+            elif (int(arg[i]) >= 0 and int(arg[i]) <= 9):
+                digits += 1
+        print(f"The text contains {l} characters:\n{upper} upper letters\n{lower} lower letters\n{punc} punctuation marks\n{space} spaces\n{digits} digits")
+
+def main():
+        try:
+            arg = sys.argv
+            l = len(arg)
+            if(l == 1):
+                arg = input("What is the text to count?\n")
+                assert (arg != "None" and arg != ""), "None or empty string entered"
+                building(arg)
+            elif(l == 2):
+                assert (arg[1] != "None" and arg[1] != ""), "None or empty string entered"
+                building(arg[1])
+            else:
+                raise Exception("Answer only one string")
+        except AssertionError as e:
+            print(f"Asssertion Error : {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            sys.exit(1)
+        
+    
+if __name__ == "__main__":
+    main()
