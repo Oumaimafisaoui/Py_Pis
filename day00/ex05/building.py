5451de4b9b7dd4c33a3dd466a049fda437ef0771
oumaimafisaoui <oumaimafisaoui@gmail.com>
2024-06-15 21:20:34 +0100

    ex05

--- day00/ex05/building.py
+++ day00/ex05/building.py
@@ -1,9 +1,6 @@
 import sys
 
 def building(arg):
-    """
-    This function takes a string and calculate the number of digits, lower case, upper case and spaces / punctuations. 
-    """
         upper = lower= digits=  punc = space = 0
         arg = arg
         l = len(arg)
@@ -16,21 +13,27 @@ def building(arg):
                 punc += 1
             elif (arg[i].isspace()):
                 space += 1
-            elif (int(arg[i]) >= 0 and int(arg[i]) <= 9):
+            elif (arg[i].isdigit()):
                 digits += 1
         print(f"The text contains {l} characters:\n{upper} upper letters\n{lower} lower letters\n{punc} punctuation marks\n{space} spaces\n{digits} digits")
 
+def handle_one_argument():
+    """Ctrl D works only when input() is waiting for an input"""
+    try:
+        line = input("What is the text to count?\n")
+        assert (line != "None" and line != ""), "None or empty string entered"
+        building(line)
+    except EOFError:
+        pass
+    except AssertionError as e:
+        print(f"Assertion error: {e}")
+
 def main():
-    """
-    This main validates the argument passed to the program, it checks the length of the argument lines and its type. 
-    """
         try:
             arg = sys.argv
             l = len(arg)
             if(l == 1):
-                arg = input("What is the text to count?\n")
-                assert (arg != "None" and arg != ""), "None or empty string entered"
-                building(arg)
+                handle_one_argument()
             elif(l == 2):
                 assert (arg[1] != "None" and arg[1] != ""), "None or empty string entered"
                 building(arg[1])
