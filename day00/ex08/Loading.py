cfc9d6c38f313a311928fca6454df06fbbe66843
oumaimafisaoui <oumaimafisaoui@gmail.com>
2024-06-16 21:28:28 +0100

    50%

--- day00/ex08/Loading.py
+++ day00/ex08/Loading.py
@@ -5,15 +5,15 @@ def ft_tqdm(lst: range) -> None:
     l = len(lst)
     for current in lst:
         yield current
-        bare_length = 100
+        bare_length = 89
         counter = int((current / l) * 101)
         filled = int((current / l) * bare_length)
         if (filled == bare_length - 1):
             bare = '=' * filled + '>'
         else:
             bare = '=' * filled + ' ' * (bare_length - filled)
-        print(f'\r {counter}% | [{bare}]', end='')
-        print(f' | {current}/{l}', end='')
+        print(f'\r{counter}%|[{bare}]', end='')
+        print(f'| {current + 1}/{l}', end='')
     print()
 
 
