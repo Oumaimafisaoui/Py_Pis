ad5c6bec5af06d7d090782f85eaf3770f2aa22ed
Oumaima Fisaoui <48260689+Oumaimafisaoui@users.noreply.github.com>
2025-12-01 23:15:17 +0000

    add ex00

--- day00/ex00/hello.py
+++ day00/ex00/hello.py
@@ -1,10 +1,17 @@
-ft_list = ["Hello","World"]
-ft_tuple = ("Hello", "Morocco")
-ft_set = {"Hello", "Khouribga!"} #convert to  a tuple
-ft_dict = {"Hello" : "1337!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
 
+ft_list[1] = 'World!'
+tmp = list(ft_tuple)
+tmp[1] = 'Morocco!'
+ft_tuple = tuple(tmp)
+ft_set.remove("tutu!")
+ft_set.add("Khouribga!")
+ft_dict["Hello"] = "1337!"
 
 print(ft_list)
 print(ft_tuple)
-print(tuple(ft_set))
+print(ft_set)
 print(ft_dict)
