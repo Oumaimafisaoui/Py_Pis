2e098c8a0ba8716a202b41e9f2c5beba5b6e8fd9
OumaimaFisaoui <oumaimafisaoui@gmail.com>
2025-12-15 00:04:59 +0100

    Enhancement: exo6 done! :shopit:

---
+++ day000/ex06/filterstring.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def filterString(arg, l):
+    words = arg.split()
+    return [word for word in words if len(word) > l]
+
+def main():
+    try:
+        l = len(sys.argv)
+        assert l == 3, "the arguments are bad"
+        assert (sys.argv[1] and sys.argv[2] and sys.argv[1] != "None" and not sys.argv[1].isdigit() and sys.argv[2].isdigit() and sys.argv[2] != 0), "the arguments are bad"
+        print(filterString(sys.argv[1], int(sys.argv[2])))
+    except AssertionError as e:
+        print(f"Assertion Error: {e}")
+
+
+
+if __name__ == "__main__":
+    main()
